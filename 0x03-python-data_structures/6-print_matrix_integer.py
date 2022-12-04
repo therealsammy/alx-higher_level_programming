41b0e2bd4b1a14308d3728f86616e512550bd107
Samuel Ohiri <therealsammy18x@gmail.com>
2022-12-04 03:11:17 -0800

    Tasks 5 - 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integers(matrix=[[]]):
-    for sublist in matrix:
-        for i in range(len(sublist)):
-            print("{:d}".format(sublist[i], end="")
-            if i is not len(sublist) - 1:
+    for sublist in range(len(matrix)):
+        for i in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if i != (len(matrix[i]) - 1):
                 print(" ", end="")
+
         print("")
